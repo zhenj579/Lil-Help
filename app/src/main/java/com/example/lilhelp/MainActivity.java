ef1bcf3fadbf0fb160052d9f356b6b9a095ba383
RN0529 <66885775+RN0529@users.noreply.github.com>
2021-01-23 21:16:45 -0500

    Home screen alterations and made the class

--- app/src/main/java/com/example/lilhelp/MainActivity.java
+++ app/src/main/java/com/example/lilhelp/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.lilhelp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
