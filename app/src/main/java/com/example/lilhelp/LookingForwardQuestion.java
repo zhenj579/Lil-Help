092e94e7bcb4733aaf8a71c4e0fa016c5ff63429
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-01-23 21:22:22 -0500

    added LookingForwardQuestion activity

---
+++ app/src/main/java/com/example/lilhelp/LookingForwardQuestion.java
@@ -0,0 +1,24 @@
+package com.example.lilhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LookingForwardQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looking_forward_question);
+        Button homeButton = (Button) findViewById(R.id.looking_forward_question_home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
