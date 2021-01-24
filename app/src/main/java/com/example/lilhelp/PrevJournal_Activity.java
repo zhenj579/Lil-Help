33cea22b03a107a1b445e7ed08279805ff465327
BexzodA <bexzodabduvaliev@gmail.com>
2021-01-23 23:27:50 -0500

    previous journals

---
+++ app/src/main/java/com/example/lilhelp/PrevJournal_Activity.java
@@ -0,0 +1,15 @@
+package com.example.lilhelp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrevJournal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prevjournal);
+    }
+
+}
