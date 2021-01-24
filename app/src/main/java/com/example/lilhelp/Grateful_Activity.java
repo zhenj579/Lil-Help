91896b89fe4988ef625deb2ce5e2cd4d7395ff8f
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-01-24 00:50:06 -0500

    implemeneted grateful activity screen

--- app/src/main/java/com/example/lilhelp/Grateful_Activity.java
+++ app/src/main/java/com/example/lilhelp/Grateful_Activity.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.time.LocalDateTime;
 
@@ -18,16 +19,62 @@ public class Grateful_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grateful_);
-
-        Button b = (Button) findViewById(R.id.backButton);
-        final Intent intent = new Intent(this, Forward_Activity.class);
-        String today = LocalDateTime.now().toString().substring(0,10); // trim off the clock portion of the date
+        final EditText et = (EditText) findViewById(R.id.gq_answer);
+        Button forward = (Button) findViewById(R.id.grateful_forward_button);
+        Button back = (Button) findViewById(R.id.grateful_back_button);
+        Button home = (Button) findViewById(R.id.grateful_question_home_button);
+        final String today = LocalDateTime.now().toString().substring(0,10); // trim off the clock portion of the date
         final Bundle bn = getIntent().getExtras();
-        JournalEntry je = (JournalEntry) bn.getSerializable(today);
-        b.setOnClickListener(new View.OnClickListener() {
+        final JournalEntry je = (JournalEntry) bn.getSerializable(today);
+        if(je.getGq() != null)
+        {
+            GratefulQuestion gq = je.getGq();
+            et.setText(gq.getAnswer());
+        }
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Forward_Activity.class);
+                String content = et.getText().toString();
+                if(je.getGq() == null)
+                {
+                    GratefulQuestion gq = new GratefulQuestion();
+                    gq.setAnswer(content);
+                    je.setGq(gq);
+                }
+                else
+                {
+                    je.getGq().setAnswer(content);
+                }
+                bn.putSerializable(today, je);
                 intent.putExtras(bn);
+                startActivity(intent);
+            }
+        });
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Accomplished_Activity.class);
+                String content = et.getText().toString();
+                if(je.getGq() == null)
+                {
+                    GratefulQuestion gq = new GratefulQuestion();
+                    gq.setAnswer(content);
+                    je.setGq(gq);
+                }
+                else
+                {
+                    je.getGq().setAnswer(content);
+                }
+                bn.putSerializable(today, je);
+                intent.putExtras(bn);
+                startActivity(intent);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
         });
