008d132328eefb2ced857ecd3729ca3deefcf4fa
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-01-24 12:30:29 -0500

    added load saved answers functionality to skip and ans question buttons

--- app/src/main/java/com/example/lilhelp/Pop.java
+++ app/src/main/java/com/example/lilhelp/Pop.java
@@ -2,12 +2,17 @@ package com.example.lilhelp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.io.FileNotFoundException;
+
 public class Pop extends Activity {
     Button home, ans, noAns;
     @Override
@@ -26,18 +31,32 @@ public class Pop extends Activity {
         int W = dm.widthPixels;
         int H = dm.heightPixels;
         getWindow().setLayout((int)(W*.8),(int)(H*.6));
+
     }
-    public void onClick(View v){
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onClick(View v) throws FileNotFoundException {
         if(v==home){
             finish();
         }
         else if(v==ans){
             Intent intent = new Intent(this, Forward_Activity.class);
+            if(DataHandler.get(getApplicationContext(), DataHandler.today) != null)
+            {
+                Bundle b = new Bundle();
+                b.putSerializable(DataHandler.today,  DataHandler.get(getApplicationContext(), DataHandler.today));
+                intent.putExtras(b);
+            }
             startActivity(intent);
         }
         else if( v==noAns){
-
+            Intent intent = new Intent(getApplicationContext(), JournalScreen.class);
+            if(DataHandler.get(getApplicationContext(), DataHandler.today) != null)
+            {
+                Bundle b = new Bundle();
+                b.putSerializable(DataHandler.today,  DataHandler.get(getApplicationContext(), DataHandler.today));
+                intent.putExtras(b);
+            }
+            startActivity(intent);
         }
-
     }
 }
