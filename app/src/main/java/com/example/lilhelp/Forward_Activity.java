65eb8de1aaa042eeeec3d025e087a9627d759dc4
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-01-24 00:49:50 -0500

    added null checking for forward activity bundle

--- app/src/main/java/com/example/lilhelp/Forward_Activity.java
+++ app/src/main/java/com/example/lilhelp/Forward_Activity.java
@@ -3,7 +3,6 @@ package com.example.lilhelp;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +20,7 @@ public class Forward_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looking_forward_question);
-        final EditText ans = (EditText) findViewById(R.id.fq_answer);
+        final EditText ans = (EditText) findViewById(R.id.gq_answer);
         final String today = LocalDateTime.now().toString().substring(0,10); // trim off the clock portion of the date
         Bundle saved = getIntent().getExtras();
         if(saved != null)
@@ -38,15 +37,27 @@ public class Forward_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Button nextButton = (Button) findViewById(R.id.forward_question_next_button);
+        Button nextButton = (Button) findViewById(R.id.looking_forward_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Grateful_Activity.class);
                 JournalEntry je = new JournalEntry();
-                je.setFq(new ForwardQuestion());
+                if(getIntent().getExtras() != null)
+                {
+                    je = (JournalEntry) getIntent().getExtras().getSerializable(today);
+                }
                 String content = ans.getText().toString();
-                je.getFq().setAnswer(content);
+                if(je.getFq() == null)
+                {
+                    ForwardQuestion fq = new ForwardQuestion();
+                    fq.setAnswer(content);
+                    je.setFq(fq);
+                }
+                else
+                {
+                    je.getFq().setAnswer(content);
+                }
                 Bundle b = new Bundle();
                 b.putSerializable(today, (Serializable) je);
                 intent.putExtras(b);
